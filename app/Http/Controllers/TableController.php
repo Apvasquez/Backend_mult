0ab7b631e3eea80390cfb2de48f610f434911a7f
Alex Paul <alex_12paul@hotmail.es>
2021-11-14 13:13:01 -0500

    subida

---
+++ app/Http/Controllers/TableController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Table;
+use Illuminate\Http\Request;
+
+class TableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function multiplicacion($number)
+    {
+        $date[] = 0;
+            $j=0;
+            for($i =1; $i <= 10 ; $i++){
+                $date[$j] = "$i * $number = ". $i* $number;
+                $j=$j+1;
+
+            }
+            return response()->json(['data'=> $date],201);
+
+    }
+    public function suma($number1,$number2)
+    {
+
+        $result= $number1 + $number2;
+
+        echo "$number1 + $number2 = ".$result ;
+
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Table  $table
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Table $table)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Table  $table
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Table $table)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Table  $table
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Table $table)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Table  $table
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Table $table)
+    {
+        //
+    }
+}
